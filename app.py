ae5660cb36672ce4ba483ff666c17615c79e1b62
Gitcomplex <bu3bounty@gmail.com>
2023-09-27 16:05:37 +0530

    added get_pdf_text function to get text

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+
+def get_pdf_text(pdf_docs):
+    text = "" 
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+
+
 
 def main():
     load_dotenv()
@@ -10,8 +24,18 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your PDFs here and click on 'Process'")
-        st.button("Process")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+            
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                
+
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                #create vector store
 
 
 
